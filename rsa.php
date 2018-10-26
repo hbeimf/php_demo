30e2ab33cd9c0233e5cc801f609285cf8b33b16d
maomao <5622876@qq.com>
2018-10-26 11:06:25 +0800

    rsa

--- rsa.php
+++ rsa.php
@@ -1,16 +1,20 @@
 <?php
 include 'vendor/autoload.php';
 
-$rsa = new \phpseclib\Crypt\RSA();
+$data = "hello world!!";
 
-// $keys = $rsa->createKey();
+//私匙加密公式解密
+openssl_private_encrypt($data, $encrypted, file_get_contents(__DIR__ . '/../api/config/privatekey.key')); //私钥加密
+$encrypted = base64_encode($encrypted); //base64只是为了避免特殊字符
+echo $encrypted . "\n"; //加密之后的结果
+openssl_public_decrypt(base64_decode($encrypted), $decrypted, file_get_contents(__DIR__ . '/../api/config/publickey.key')); //私钥加密的内容通过公钥可用解密出来
+var_dump($decrypted);
 
-// extract($keys);
+// exit;
 
-// echo base64_encode($ciphertext) . "\n";
-
-$str = "mq/JeBBgQQK3L8FeMsRhw8VtTcWlrPjxeYmL5y67lWIyYzmk7maGhWP0fFsrkiP5FEMZVo+hOm+VjIn0O3pZLNCzDZfNoeb6SwfM61lRIGEN2gM73DTyQMLCerEKrj5znbG+3i+FmbwCwIo7i0HHuhVA+/IVjWoPXNDv8QWdUgM=";
-
-$rsa->loadKey(file_get_contents(__DIR__ . '/../api/config/publickey.key'));
-echo $rsa->decrypt(base64_decode($str));
-echo "\n";
+// //公私加密私匙解密
+// openssl_public_encrypt($data, $encrypted, $pu_key); //公钥加密
+// $encrypted = base64_encode($encrypted); //base64只是为了避免特殊字符
+// // echo $encrypted;exit;//加密之后的结果
+// openssl_private_decrypt(base64_decode($encrypted), $decrypted, $pi_key); //私钥解密
+// // echo $decrypted,"\n";  //解密之后的结果
