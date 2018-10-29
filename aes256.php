6addd574d102926dde5ae56de829295cc0acde52
maomao <5622876@qq.com>
2018-10-29 14:26:49 +0800

    256

---
+++ aes256.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * AES.php
+ * @author Andrey Izman <izmanw@gmail.com>
+ * @copyright Andrey Izman (c) 2018
+ * @license MIT
+ */
+// namespace mervick\aesEverywhere;
+/**
+ * Class AES256
+ * @package mervick\aesEverywhere
+ */
+class AES256 {
+	/**
+	 * Encrypt string
+	 *
+	 * @param string|numeric $text
+	 * @param string $passphrase
+	 * @return string
+	 * @throws \Exception
+	 */
+	public static function encrypt($text, $passphrase) {
+		$salt = openssl_random_pseudo_bytes(8);
+		$salted = $dx = '';
+		while (strlen($salted) < 48) {
+			$dx = md5($dx . $passphrase . $salt, true);
+			$salted .= $dx;
+		}
+		$key = substr($salted, 0, 32);
+		$iv = substr($salted, 32, 16);
+		return base64_encode('Salted__' . $salt . openssl_encrypt($text . '', 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv));
+	}
+	/**
+	 * Decrypt string
+	 *
+	 * @param string $encrypted
+	 * @param string $passphrase
+	 * @return string|numeric
+	 * @throws \Exception
+	 */
+	public static function decrypt($encrypted, $passphrase) {
+		$encrypted = base64_decode($encrypted);
+		$salted = substr($encrypted, 0, 8) == 'Salted__';
+		if (!$salted) {
+			return null;
+		}
+		$salt = substr($encrypted, 8, 8);
+		$encrypted = substr($encrypted, 16);
+		$salted = $dx = '';
+		while (strlen($salted) < 48) {
+			$dx = md5($dx . $passphrase . $salt, true);
+			$salted .= $dx;
+		}
+		$key = substr($salted, 0, 32);
+		$iv = substr($salted, 32, 16);
+		return openssl_decrypt($encrypted, 'aes-256-cbc', $key, true, $iv);
+	}
+}
+
+$password = "asdfghkl;'][poi?";
+
+// encryption
+$encode = base64_encode(AES256::encrypt('TEXT', $password));
+
+echo "encode: {$encode}\n";
+
+// decryption
+$bin = base64_decode($encode);
+$decode = AES256::decrypt($bin, $password);
+
+echo "decode: {$decode}\n";
