82e0c47ea1244b63b292e367b2e4363d0fcceae1
maomao <5622876@qq.com>
2018-10-30 14:19:20 +0800

    isWsClient

--- ws.php
+++ ws.php
@@ -15,6 +15,30 @@ $server->on('close', function ($ser, $fd) {
 	echo "client {$fd} closed\n";
 });
 
+// 广播消息
+// http://localhost:9501/?message=xxxx
+$server->on('request', function (swoole_http_request $request, swoole_http_response $response) {
+	global $server; //调用外部的server
+	// $server->connections 遍历所有websocket连接用户的fd，给所有用户推送
+	foreach ($server->connections as $fd) {
+		if (isWsClient($fd)) {
+			$server->push($fd, $request->get['message']);
+		}
+
+	}
+});
+
 $server->start();
+
+function isWsClient($fd) {
+	global $server;
+	$info = $server->connection_info($fd);
+
+	if ($info && isset($info["websocket_status"]) && intval($info["websocket_status"]) > 0) {
+		return true;
+	}
+
+	return false;
+}
 
 ?>
